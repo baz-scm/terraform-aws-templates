dab4d4a99dca188d24f645d7026fcfdf896d0a7f
nimrodkor <nimrod@baz.co>
2026-04-26 21:42:35 +0300

    Tested

--- tests/pulumi/private-spec-reviewer/index.ts
+++ tests/pulumi/private-spec-reviewer/index.ts
@@ -1,46 +1,28 @@
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 import { PrivateSpecReviewer } from "@baz/pulumi-private-spec-reviewer";
 
-// VPC mirroring tests/private-spec-reviewer/main.tf:
-// 3 AZs, private + public subnets, single NAT gateway
-const vpc = new awsx.ec2.Vpc("private-spec-reviewer-test-vpc", {
-    cidrBlock: "10.0.0.0/16",
-    numberOfAvailabilityZones: 3,
-    subnetSpecs: [
-        { type: awsx.ec2.SubnetType.Private, cidrMask: 24 },
-        { type: awsx.ec2.SubnetType.Public, cidrMask: 24 },
-    ],
-    natGateways: { strategy: awsx.ec2.NatGatewayStrategy.Single },
-    enableDnsHostnames: true,
-    tags: { Environment: "test", Module: "private-spec-reviewer" },
-});
-
-// SSM parameters holding test credentials
-const usernameParam = new aws.ssm.Parameter("test-username", {
-    name: "/bazai/preview/username",
-    type: aws.ssm.ParameterType.SecureString,
-    value: "test-user",
-});
-
-const passwordParam = new aws.ssm.Parameter("test-password", {
-    name: "/bazai/preview/password",
-    type: aws.ssm.ParameterType.SecureString,
-    value: "test-password",
-});
+// To test with VPC + PrivateLink, replace IDs and use this instead:
+// const reviewer = new PrivateSpecReviewer("private-spec-reviewer", {
+//     bazAwsAccountId: "647348643223",
+//     ssmUsernamePath: "/bazai/preview/username",
+//     ssmPasswordPath: "/bazai/preview/password",
+//     region: "eu-central-1",
+//     enableVpc: true,
+//     vpcId: "vpc-xxxxxxxxx",
+//     subnetIds: ["subnet-xxx", "subnet-yyy", "subnet-zzz"],
+//     previewEnvCidr: "10.0.0.0/16",
+//     enablePrivateLink: true,
+//     tags: { Env: "test" },
+// });
 
 const reviewer = new PrivateSpecReviewer("private-spec-reviewer", {
     bazAwsAccountId: "647348643223",
-    ssmUsernamePath: usernameParam.name,
-    ssmPasswordPath: passwordParam.name,
+    ssmUsernamePath: "/bazai/preview/username",
+    ssmPasswordPath: "/bazai/preview/password",
     region: "eu-central-1",
-    enableVpc: true,
-    vpcId: vpc.vpcId,
-    subnetIds: vpc.privateSubnetIds,
-    previewEnvCidr: "10.0.0.0/16",
-    enablePrivateLink: true,
+    enableVpc: false,
+    enablePrivateLink: false,
     tags: { Env: "test" },
-}, { dependsOn: [usernameParam, passwordParam] });
+});
 
 export const crossAccountRoleArn = reviewer.crossAccountRoleArn;
 export const externalId = reviewer.externalId;
